5ecb4bc84574829fbf7dfe63d154012ce6d9d3e7
stackinspector <stackinspector@126.com>
2023-03-23 20:30:55 +0800

    fixed buf

--- main.rs
+++ main.rs
@@ -1,13 +1,11 @@
-fn once(key: &[u8], bytes: &[u8]) -> Vec<u8> {
+fn once<const N: usize>(key: &[u8], ibuf: &[u8; N], obuf: &mut [u8; N]) {
     use tiny_keccak::{Shake, Hasher, Xof};
     let mut hasher = Shake::v256();
     hasher.update(key);
-    let mut output = vec![0; bytes.len()];
-    hasher.squeeze(&mut output);
-    for i in 0..bytes.len() {
-        output[i] = output[i] ^ bytes[i];
+    hasher.squeeze(obuf);
+    for i in 0..ibuf.len() {
+        obuf[i] = obuf[i] ^ ibuf[i];
     }
-    output
 }
 
 fn main() {
@@ -25,7 +23,17 @@ fn main() {
         key: String,
     }
 
-    use std::{fs::{read, write}, path::PathBuf};
+    use std::{fs::OpenOptions, io::{Read, Write}, path::PathBuf};
     let Args { input, output, key } = argh::from_env();
-    write(output, once(key.as_bytes(), &read(input).unwrap())).unwrap();
+
+    let mut input = OpenOptions::new().read(true).open(input).unwrap();
+    let mut output = OpenOptions::new().read(true).open(output).unwrap();
+
+    const BUF_LEN: usize = 16384;
+    let mut ibuf = [0; BUF_LEN];
+    let mut obuf = [0; BUF_LEN];
+
+    input.read_exact(&mut ibuf).unwrap();
+    once(key.as_bytes(), &ibuf, &mut obuf);
+    output.write_all(&obuf).unwrap();
 }
