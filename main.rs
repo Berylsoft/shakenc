92c605b56e762646e3d80d2bd2f908f4f332802e
stackinspector <stackinspector@126.com>
2023-03-23 20:52:29 +0800

    dyn buf

--- main.rs
+++ main.rs
@@ -6,7 +6,8 @@ fn init(key: &[u8]) -> Shake {
     ctx
 }
 
-fn once<const N: usize>(ctx: &mut Shake, ibuf: &[u8; N], obuf: &mut [u8; N]) {
+fn once(ctx: &mut Shake, ibuf: &[u8], obuf: &mut [u8]) {
+    assert_eq!(ibuf.len(), obuf.len());
     ctx.squeeze(obuf);
     for i in 0..ibuf.len() {
         obuf[i] = obuf[i] ^ ibuf[i];
@@ -26,18 +27,20 @@ fn main() {
         /// key
         #[argh(option, short = 'k')]
         key: String,
+        /// buffer size (KB, default 16KB, will take twice this size of runtime memory)
+        #[argh(option)]
+        buf: Option<usize>,
     }
 
     use std::{fs::OpenOptions, io::{Read, Write}, path::PathBuf};
-    let Args { input, output, key } = argh::from_env();
+    let Args { input, output, key, buf: buf_len } = argh::from_env();
 
     let mut ctx = init(key.as_bytes());
     let mut input = OpenOptions::new().read(true).open(input).unwrap();
     let mut output = OpenOptions::new().read(true).open(output).unwrap();
-
-    const BUF_LEN: usize = 16384;
-    let mut ibuf = [0; BUF_LEN];
-    let mut obuf = [0; BUF_LEN];
+    let buf_len = buf_len.and_then(|n| Some(n * 1024)).unwrap_or(16384);
+    let mut ibuf = vec![0u8; buf_len];
+    let mut obuf = vec![0u8; buf_len];
 
     input.read_exact(&mut ibuf).unwrap();
     once(&mut ctx, &ibuf, &mut obuf);
