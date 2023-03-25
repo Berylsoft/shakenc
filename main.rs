e66c71976a89a54bf5520a442eb3f1cebedf79e2
stackinspector <stackinspector@126.com>
2023-03-25 21:34:14 +0800

    hash

--- main.rs
+++ main.rs
@@ -1,24 +1,39 @@
 use tiny_keccak::{CShake, Hasher as _, Xof as _};
 
 struct Context {
-    inner: CShake,
+    cipher: CShake,
+    ihash: CShake,
+    ohash: CShake,
 }
 
-impl Context {
-    const CIPHER_CUSTOM: &str = "__shakenc__file-stream-cipher";
+const CIPHER_CUSTOM: &str = "__shakenc__file-stream-cipher";
+const HASH_CUSTOM: &str = "__shakenc__file-hash";
 
+impl Context {
     fn init(key: &[u8]) -> Self {
-        let mut inner = CShake::v256(&[], Self::CIPHER_CUSTOM.as_bytes());
-        inner.update(key);
-        Self { inner }
+        let mut cipher = CShake::v256(&[], CIPHER_CUSTOM.as_bytes());
+        cipher.update(key);
+        let ihash = CShake::v256(&[], HASH_CUSTOM.as_bytes());
+        let ohash = CShake::v256(&[], HASH_CUSTOM.as_bytes());
+        Self { cipher, ihash, ohash }
     }
 
     fn next(&mut self, ibuf: &[u8], obuf: &mut [u8]) {
         assert_eq!(ibuf.len(), obuf.len());
-        self.inner.squeeze(obuf);
+        self.ihash.update(ibuf);
+        self.ohash.update(obuf);
+        self.cipher.squeeze(obuf);
         for i in 0..ibuf.len() {
             obuf[i] ^= ibuf[i];
         }
+    }
+
+    fn finish<const N: usize>(self) -> ([u8; N], [u8; N]) {
+        let mut ihash = [0; N];
+        let mut ohash = [0; N];
+        self.ihash.finalize(&mut ihash);
+        self.ohash.finalize(&mut ohash);
+        (ihash, ohash)
     }
 }
 
@@ -49,20 +64,23 @@ fn main() {
     let mut ctx = Context::init(key.as_bytes());
     let mut ibuf = vec![0u8; buf_len];
     let mut obuf = vec![0u8; buf_len];
-    let mut input = OpenOptions::new().read(true).open(input).unwrap();
-    let mut output = OpenOptions::new().create_new(true).write(true).open(output).unwrap();
+    let mut ifile = OpenOptions::new().read(true).open(input).unwrap();
+    let mut ofile = OpenOptions::new().create_new(true).write(true).open(output).unwrap();
 
     loop {
-        let read_len = input.read(&mut ibuf).unwrap();
+        let read_len = ifile.read(&mut ibuf).unwrap();
         if read_len == 0 {
             // TODO check if EOF
+            let (ihash, ohash) = ctx.finish::<32>();
+            println!("input  hash: {}", hex::encode(ihash));
+            println!("output hash: {}", hex::encode(ohash));
             break;
         } else if read_len == buf_len {
             ctx.next(&ibuf, &mut obuf);
-            output.write_all(&obuf).unwrap();
+            ofile.write_all(&obuf).unwrap();
         } else {
             ctx.next(&ibuf[0..read_len], &mut obuf[0..read_len]);
-            output.write_all(&obuf[0..read_len]).unwrap();
+            ofile.write_all(&obuf[0..read_len]).unwrap();
         }
     }
 }
